cf490280eae845c9301f94a78280e430855b355b
Nachiket-git-dev <nachiketchaitanya.ssl22@gmail.com>
2020-12-14 16:24:51 +0530

    error changes

--- server/route/index.js
+++ server/route/index.js
@@ -11,7 +11,9 @@ var cors = require('cors');
 var router = require('express').Router();
 const app=express();
 router.use(function(req, res, next){
-   
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
     next();
 });
 router.use(cors({ Origin: '*' }));
